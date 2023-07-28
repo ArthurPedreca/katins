b49723a9484112e80fc67c07ed176dd5c1c90415
pedreca <80847947+ArthurPedreca@users.noreply.github.com>
2023-07-28 19:22:01 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let cards = document.querySelectorAll(".card");
-cards.forEach(card => {
-    card.addEventListener("click", function() {
-        let productId = this.getAttribute("data-id");
-        window.location.href = "product.html?id=" + productId;
-    });
-});
